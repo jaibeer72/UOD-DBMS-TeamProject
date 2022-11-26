c2cb271ddee898e1c150c519b3569d8cdb562015
Jaibeer  S Dugal <jaibeer72@gmail.com>
2022-11-26 15:28:46 +0000

    TODO added for each of the statements

---
+++ SQL Queries/Customer.sql
@@ -0,0 +1,13 @@
+-- Active: 1666003760240@@Jaibeers-MBP.wireless.dundee.ac.uk@3306@22ac5d04
+
+-- Create new customer
+
+-- Get Customer information
+
+-- Create Customer view for resteraunts
+
+-- Create Customer view for selected resteratunts 
+
+-- Customer create new order
+
+-- Customer Check out 
---
+++ SQL Queries/Resteraunts.sql
@@ -0,0 +1,11 @@
+-- Active: 1666003760240@@Jaibeers-MBP.wireless.dundee.ac.uk@3306@22ac5d04
+
+-- Create new resteraunt 
+-- Add food item 1 
+-- add multiple food items at once ? i don't know if that's possible if not let's create a new fild for insertions 
+-- Create resteraunt view to see 
+-- Current ordered available to accept
+-- all older orders
+-- add food items
+-- make food items unavailable
+-- delete food items.
