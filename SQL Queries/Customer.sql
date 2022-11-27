24c1822476364abf953d7f2163a1634a89afbe31
Jaibeer  S Dugal <jaibeer72@gmail.com>
2022-11-27 18:03:00 +0000

    viewsx with a bit of finess to make sure to get current city resteraunts so no repeat code

--- SQL Queries/Customer.sql
+++ SQL Queries/Customer.sql
@@ -1,14 +1,63 @@
 -- Active: 1666003760240@@Jaibeers-MBP.wireless.dundee.ac.uk@3306@22ac5d04
 
 -- Create new customer
-INSERT INTO `customer`(customer_name,customer_city,customer_email,customer_userId,customer_password) 
-    VALUES('Jai','London','jaiLon@test.com','')
+
+INSERT INTO
+    customer(
+        customer.customer_name,
+        customer.customer_city,
+        customer.customer_email,
+        customer.customer_userId
+    )
+VALUES (
+        "Jaibeer",
+        "Dundee",
+        "jaibeer2test@gmail.com",
+        "jaibeer2"
+    );
+
 -- Get Customer information
 
--- Create Customer view for resteraunts
+SELECT * from customer WHERE customer.customer_name = "Jaibeer";
 
--- Create Customer view for selected resteratunts 
+-- Update information
+
+-- Delete
+
+-- Create Customer view for resteraunts based on the city of the customer
+
+CREATE OR REPLACE VIEW CUSTOMERALLRESTERAUNTS AS 
+	SELECT
+	    restaurants.restaurant_id,
+	    restaurants.restaurant_name,
+	    restaurants.restaurant_city,
+	    foodList.food_name,
+	    foodList.food_price,
+	    foodList.food_description,
+	    foodList.food_status
+	FROM restaurants
+	    CROSS JOIN foodList ON restaurants.restaurant_id = foodList.r
+RESTAURANT_ID; 
+
+SELECT * FROM customerallresteraunts;
+
+DELIMITER //
+
+CREATE PROCEDURE GETCURRENTCITYRESTARAUNTS(IN CITY 
+VARCHAR(255)) BEGIN 
+	SELECT *
+	FROM customerallresteraunts
+	WHERE
+	    customerallresteraunts.restaurant_city = city;
+	END// 
+
+
+DELIMITER;
+
+CALL GetCurrentCityRestaraunts("Dundee");
+
+-- Create Customer view for selected resteratunts
 
 -- Customer create new order
 
--- Customer Check out 
+-- Customer Check out
